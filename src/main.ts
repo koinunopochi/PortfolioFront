abf8c0327bda24fa9032e1a80b9810d74f3e9f70
okayama <okapi771205a@gmail.com>
2023-09-22 15:44:01 +0900

    vuetifyの内容を追加

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify のインポートを追加 **********/
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// 以下を追加 **********/
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+
+// 以下のように .use(vuetify) を追加
+createApp(App).use(vuetify).mount('#app')
