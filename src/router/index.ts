c57ddc5a8c60e09925a7f60adb9614c8a0be03c7
okayama <okapi771205a@gmail.com>
2023-09-22 15:31:43 +0900

    名前の変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 // import HomeView from '../views/HomeView.vue'
-import IndexView from '../views/IndexView.vue'
+import IndexView from '../views/IndexView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +16,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutMeView.vue'),
+      component: () => import('../views/AboutMe.vue'),
     },
     // {
     //   path: '/index',
@@ -26,4 +26,4 @@ const router = createRouter({
   ],
 });
 
-export default router
+export default router;
