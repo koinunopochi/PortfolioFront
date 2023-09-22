40efd635ea85794af6b76de030938b20f93b77fd
okayama <okapi771205a@gmail.com>
2023-09-22 15:29:14 +0900

    使用しない要素のコメントアウト

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import IndexView from '../views/IndexView.vue'
 
 const router = createRouter({
@@ -8,23 +8,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: IndexView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/about-me',
+      name: 'about-me',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutMeView.vue'),
     },
-    {
-      path: '/index',
-      name: 'index',
-      component: IndexView
-    }
-
-  ]
-})
+    // {
+    //   path: '/index',
+    //   name: 'index',
+    //   component: IndexView,
+    // },
+  ],
+});
 
 export default router
