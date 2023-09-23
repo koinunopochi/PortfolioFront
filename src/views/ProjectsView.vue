c7219a802da8be33f161862e21a8f31aae09b695
okayama <okapi771205a@gmail.com>
2023-09-24 01:13:42 +0900

    最低限の仮置き

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="all-contents">
+        <!-- サイドバー -->
+    <SideBar />
+    <div class="projects">
+      <h1 class="title">PROJECTS</h1>
+      <div class="contents">
+        <h2>プロジェクト</h2>
+        <p>現在、プロジェクトはありません。</p>
+      </div>
+    </div>
+    <TableOfContents :items="tableOfContentsItems"/>
+  </div>
+</template>
+<script setup lang="ts">
+import SideBar from '../components/SideBar.vue';
+import TableOfContents from '../components/TableOfContents.vue';
+
+const tableOfContentsItems = [
+  {
+    id: 'profile',
+    name: 'プロフィール',
+  },
+];
+</script>
