974aaf505e55364a9d33f0a2810a9b85603cba61
okayama <okapi771205a@gmail.com>
2023-09-24 01:13:05 +0900

    プロジェクトルートの作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 // import HomeView from '../views/HomeView.vue'
 import IndexView from '../views/IndexView.vue';
+import ProjectsViewVue from '@/views/ProjectsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: IndexView,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsViewVue,
     },
     {
       path: '/about-me',
