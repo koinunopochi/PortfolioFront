a1147ea236f3210fd668e5cd5fd69069c9892657
okayama <okapi771205a@gmail.com>
2023-09-24 02:46:57 +0900

    おしゃれな感じに変更

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="not-found">
-    <h1>404 Not Found</h1>
-    <p>お探しのページは見つかりませんでした。</p>
-    <p>URLをご確認の上、再度アクセスしてください。</p>
-    
-  </div>
+  <v-container class="not-found-container" fluid>
+    <v-row class="fill-height" align="center" justify="center">
+      <v-col class="text-center">
+        <v-icon size="150" class="mb-4">mdi-alert-circle-outline</v-icon>
+        <h1 class="mb-6">404: Page Not Found</h1>
+        <v-btn color="primary" to="/">Return to Home</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
