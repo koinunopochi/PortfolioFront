0fe1ebd55ae51606ed4cb3fb09938705d6e0107e
okayama <okapi771205a@gmail.com>
2023-09-23 17:51:40 +0900

    サイドバーの作成

---
+++ src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="side-bar">
+    <p>hogehoge</p>
+    <img src="" alt="x" />
+    <img src="" alt="qiita" />
+  </div>
+</template>
+<style scoped>
+.side-bar {
+  /* マージンの設定 */
+  margin: 20px;
+  /* 横幅 */
+  width: 20%;
+  /* 最小サイズ */
+  min-width: 30px;
+  /* 高さ */
+  height: 100vh;
+  /* パディングの設定 */
+  padding: 10px;
+  /* 右寄りに配置 */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
