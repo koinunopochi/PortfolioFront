7faa3767b61e0281442c46793702714e46cff1ac
okayama <okapi771205a@gmail.com>
2023-09-23 21:55:12 +0900

    それっぽい見た目に調整（仮置き）

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="nav-bar">
-    <a href="">About me</a>
+    <a href="/">HOME</a>
+    <a href="/about-me">ABOUT ME</a>
   </div>
 </template>
+
 <style scoped>
-.nav-bar{
+.nav-bar {
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
   width: 100%;
   height: 50px;
   background-color: #fff;
+  padding-top: 10px;
+  padding-right: 20px;
+}
+.nav-bar a {
+  margin-right: 20px;
+  font-size: 20px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #545555;
+  transition: color 0.3s, text-decoration 0.3s;
+}
+.nav-bar a:hover {
+  text-decoration: underline;
 }
 </style>
