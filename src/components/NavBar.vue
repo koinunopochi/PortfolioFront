32ae3bbe5bef7805291f237070d8c1a069a819b4
okayama <okapi771205a@gmail.com>
2023-09-23 18:00:03 +0900

    仮置き用のNavBarを作成

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="nav-bar">
+    <a href="">About me</a>
+  </div>
+</template>
+<style scoped>
+.nav-bar{
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+}
+</style>
