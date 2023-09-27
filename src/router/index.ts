62c5fd5a44176f814e06333379859c645ed49a72
okayama <okapi771205a@gmail.com>
2023-09-27 22:16:47 +0900

    作成中と示すページを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import IndexView from '../views/IndexView.vue';
 import ProjectsViewVue from '@/views/ProjectsView.vue';
 import AutoPic from '@/views/projects/AutoPic.vue';
 import NotFound from '@/views/NotFound.vue';
+import UnderConstructionVue from '@/views/UnderConstruction.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutMe.vue'),
+    },
+    {
+      path: '/projects/youtube-text',
+      name: 'youtube-text',
+      component: UnderConstructionVue,
+    },
+    {
+      path: '/projects/portfolio',
+      name: 'portfolio',
+      component: UnderConstructionVue,
+    },
+    {
+      path: '/projects/node-summary',
+      name: 'node-summary',
+      component: UnderConstructionVue,
     },
     {
       path: '/:pathMatch(.*)*',
---
+++ src/views/UnderConstruction.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col class="text-center">
+        <v-icon large color="warning">mdi-alert</v-icon>
+        <h1 class="headline">このページはまだ作成中です</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UnderConstruction',
+};
+</script>
