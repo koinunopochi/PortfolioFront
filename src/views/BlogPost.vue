f26e57835f581233f3a42a51c49770e98a4181f1
okayama <okapi771205a@gmail.com>
2023-10-05 20:21:54 +0900

    md入力ができるように変更

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,42 +1,64 @@
 <!-- BlogPost.vue -->
 <template>
-  <div class="main">
-    <h1>BlogPost</h1>
-    <div class="form">
-      <div class="form-item">
-        <label for="title">タイトル</label>
-        <input type="text" id="title" v-model="title" />
+  <div class="all-content">
+    <div class="main">
+      <h1>BlogPost</h1>
+      <div class="form">
+        <div class="form-item">
+          <label for="title">タイトル</label>
+          <input type="text" id="title" v-model="title" />
+        </div>
+        <div class="form-item">
+          <label for="description">概要</label>
+          <input type="text" id="description" v-model="description" />
+        </div>
+        <div class="form-item">
+          <label for="content">本文</label>
+          <textarea id="content" v-model="content"></textarea>
+        </div>
+        <div class="form-item">
+          <button @click="submit">送信</button>
+          <p class="error-message">{{ error_message }}</p>
+        </div>
       </div>
-      <div class="form-item">
-        <label for="description">概要</label>
-        <input type="text" id="description" v-model="description" />
-      </div>
-      <div class="form-item">
-        <label for="content">本文</label>
-        <textarea id="content" v-model="content"></textarea>
-      </div>
-      <div class="form-item">
-        <button @click="submit">送信</button>
-      </div>
+    </div>
+    <div class="main md">
+      <h1 v-if="title != ''">{{ title }}</h1>
+      <h3 v-if="description != ''">概要</h3>
+      <p>{{ description }}</p>
+      <div v-html="convertedMarkdown"></div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { easyFetch } from '../utils/submit';
-
+import { marked } from 'marked';
 const apiUrl = import.meta.env.VITE_APP_API_DOMAIN;
 
 const title = ref('');
 const description = ref('');
 const content = ref('');
 
-const submit = async() => {
-  try{
-    const res = await easyFetch('POST', new URL(apiUrl+'/blog'), {
+const error_message = ref('');
+
+const isCheckContents = () => {
+  if (title.value == '' || description.value == '' || content.value == '') {
+    return false;
+  }
+  return true;
+};
+
+const submit = async () => {
+  try {
+    if (!isCheckContents()) {
+      error_message.value = ' この値は必須です';
+      return;
+    }
+    const res = await easyFetch('POST', new URL(apiUrl + '/blog'), {
       title: title.value,
-      description: description.value,
-      content: content.value
+      overview: description.value,
+      content: content.value,
     });
     if (res.ok) {
       console.log('success');
@@ -47,19 +69,40 @@ const submit = async() => {
     console.error('Failed to fetch contents', error);
   }
 };
+const convertedMarkdown = ref('');
+
+// watchとの違いが不明
+watchEffect(() => {
+  convertedMarkdown.value = marked(content.value);
+});
 </script>
 <style scoped>
-.main {
-  background-color: #fff; /* Mainの背景色 */
-  padding: 20px; /* パディングを適用 */
-  border-radius: 10px; /* 角を丸める */
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1); /* 影をつける */
-  max-width: 600px; /* 最大幅を設定 */
-  margin: auto; /* 中央寄せ */
-  margin-top: 50px;
+.all-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch; /* これを追加 */
+  gap: 20px;
+  max-width: 90%;
+  margin: 50px auto 0 auto;
 }
+
+.main,
+.md {
+  flex: 1;
+  min-width: 0;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+}
+
+.md {
+  align-self: stretch; /* これを追加 */
+}
+
 input[type='text'],
-textarea{
+textarea {
+  font-size: 10px;
   width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
@@ -67,13 +110,16 @@ textarea{
   box-sizing: border-box;
   margin-bottom: 20px;
 }
+textarea {
+  height: 200px;
+}
 .form-item {
   margin-bottom: 20px; /* form-itemの間隔を設定 */
 }
 
 button {
   padding: 10px 20px; /* ボタンのパディングを設定 */
-  background-color: #4CAF50; /* ボタンの背景色を設定 */
+  background-color: #4caf50; /* ボタンの背景色を設定 */
   color: white; /* テキストの色を設定 */
   border: none; /* ボーダーを無効に */
   cursor: pointer; /* カーソルをポインターに */
@@ -84,4 +130,10 @@ button {
 button:hover {
   background-color: #45a049; /* ホバー時の背景色を設定 */
 }
+.error-message{
+  color: red;
+}
+</style>
+<style scoped>
+@import '../assets/main.css';
 </style>
