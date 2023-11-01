5ca475dd8e5c454fe82169819479eac8a9032d77
okayama <okapi771205a@gmail.com>
2023-11-02 01:47:21 +0900

    仮で、作成

--- src/views/auth/DeleteAccount.vue
+++ src/views/auth/DeleteAccount.vue
@@ -1,1 +1,36 @@
 <!-- DeleteAccount.vue -->
+<template>
+  <table>
+    <tr>
+      <th>username</th>
+      <th>role</th>
+    </tr>
+    <tr v-for="user in users" :key="user.username">
+      <td>{{ user.username }}</td>
+      <td>{{ user.role }}</td>
+    </tr>
+  </table>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { easyFetch } from '../../utils/submit';
+
+const users = ref(
+  [] as {
+    username: string;
+    role: string;
+  }[]
+);
+
+const apiUrl = import.meta.env.VITE_APP_API_DOMAIN;
+
+const getUsers = async () => {
+  const res = await easyFetch('GET', new URL(apiUrl + '/auth/user'));
+  users.value = res.data.userInfo; // ここで 'userInfo' を指定します。
+};
+
+onMounted(() => {
+  getUsers();
+});
+</script>
